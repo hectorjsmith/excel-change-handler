a95c32a2d017b6636b83eb2a1595d1f6e61b2e0c
Hector <dev@hsmith.org>
2020-05-11 11:00:42 +0100

    #2: Setup basic library API
    Add interfaces and classes for the library API itself and the base Excel
    data structures the library cares about (sheets and ranges).

---
+++ CSharpExcelChangeLogger/Api/ChangeLoggerApi.cs
@@ -0,0 +1,34 @@
+﻿using CSharpExcelChangeLogger.Excel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpExcelChangeLogger.Api
+{
+    public interface IChangeLoggerApi
+    {
+        void BeforeChange(IWorksheet sheet, IRange range);
+
+        void AfterChange(IWorksheet sheet, IRange range);
+    }
+
+    public class ChangeLoggerApi : IChangeLoggerApi
+    {
+        private static IChangeLoggerApi? _instance;
+        public static IChangeLoggerApi Instance = _instance ?? (_instance = new ChangeLoggerApi());
+
+        private ChangeLoggerApi()
+        {
+        }
+
+        public void BeforeChange(IWorksheet sheet, IRange range)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AfterChange(IWorksheet sheet, IRange range)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CSharpExcelChangeLogger/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CSharpExcelChangeLogger
-{
-    public class Class1
-    {
-    }
-}
