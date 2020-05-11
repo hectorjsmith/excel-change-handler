1dc747e1568ab2617444a4fc3969e3574ea339c3
Hector <dev@hsmith.org>
2020-05-11 11:13:09 +0100

    #2: Add support for injecting a logger
    Update the library to allow injecting a logger interface. The purpose is
    to have the entire library use that logger interface to log messages to
    the client code.

--- CSharpExcelChangeLogger/Api/ChangeLoggerApi.cs
+++ CSharpExcelChangeLogger/Api/ChangeLoggerApi.cs
@@ -1,4 +1,6 @@
-﻿using CSharpExcelChangeLogger.Excel;
+﻿using CSharpExcelChangeLogger.Base;
+using CSharpExcelChangeLogger.Excel;
+using CSharpExcelChangeLogger.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +9,8 @@ namespace CSharpExcelChangeLogger.Api
 {
     public interface IChangeLoggerApi
     {
+        void SetLogger(ILogger? logger);
+
         void BeforeChange(IWorksheet sheet, IRange range);
 
         void AfterChange(IWorksheet sheet, IRange range);
@@ -19,6 +23,11 @@ namespace CSharpExcelChangeLogger.Api
 
         private ChangeLoggerApi()
         {
+        }
+
+        public void SetLogger(ILogger? logger)
+        {
+            StaticChangeLoggerManager.SetInjectedLogger(logger);
         }
 
         public void BeforeChange(IWorksheet sheet, IRange range)
---
+++ CSharpExcelChangeLogger/Base/StaticChangeLoggerManager.cs
@@ -0,0 +1,24 @@
+﻿using CSharpExcelChangeLogger.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpExcelChangeLogger.Base
+{
+    internal static class StaticChangeLoggerManager
+    {
+        private static readonly ILogger _inactiveLogger = new InactiveLogger();
+        private static ILogger? _injectedLogger;
+
+        public static ILogger Logger
+        {
+            get { return _injectedLogger ?? _inactiveLogger; }
+        }
+
+        public static void SetInjectedLogger(ILogger? logger)
+        {
+            _injectedLogger = logger;
+        }
+
+    }
+}
