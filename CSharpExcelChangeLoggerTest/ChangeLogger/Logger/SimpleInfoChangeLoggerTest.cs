50cb5b3807113735121000210560cc065fd38ad2
Hector <dev@hsmith.org>
2020-05-15 19:23:36 +0100

    #18: Add new test methods
    Add new test to ensure the simple logger logs detected changes.
    Add new test to ensure the change processor calls all available change
    handlers when a change is detected.

---
+++ CSharpExcelChangeLoggerTest/ChangeLogger/Logger/SimpleInfoChangeLoggerTest.cs
@@ -0,0 +1,24 @@
+﻿using CSharpExcelChangeLogger.ChangeLogger.Handler;
+using CSharpExcelChangeLogger.ChangeLogger.Logger;
+using CSharpExcelChangeLoggerTest.Mock;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpExcelChangeLoggerTest.ChangeLogger.Logger
+{
+    class SimpleInfoChangeLoggerTest
+    {
+        [Test]
+        public void Given_SimpleChangeLogger_When_HandleChangeMethodCalled_Then_ChangeIsLogged()
+        {
+            TestLogger logger = new TestLogger();
+            
+            IChangeHandler highlighter = new SimpleInfoChangeLogger(logger);
+            highlighter.HandleChange(new SimpleMockMemoryComparison(), new SimpleMockSheet(), new SimpleMockRange());
+
+            Assert.AreEqual(1, logger.InfoMessageCount, "One info message should be logged for a change");
+        }
+    }
+}
