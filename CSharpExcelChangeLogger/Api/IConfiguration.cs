73838825845f2f8069b2564c7cc5babc841c6c08
Hector <dev@hsmith.org>
2020-05-26 06:37:21 +0000

    #22: Add documentation to public interfaces
    Update the public API and Configuration interfaces to include more
    documentation on what the various methods and properties are for.

--- CSharpExcelChangeLogger/Api/IConfiguration.cs
+++ CSharpExcelChangeLogger/Api/IConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +6,10 @@ namespace CSharpExcelChangeLogger.Api
 {
     public interface IConfiguration
     {
+        /// <summary>
+        /// Global flag to enable/disable change detection.
+        /// If this field is set to false, no data is saved to memory in the BeforeChange method and no handlers are called in the AfterChange method.
+        /// </summary>
         bool ChangeHandlingEnabled { get; set; }
     }
 }
