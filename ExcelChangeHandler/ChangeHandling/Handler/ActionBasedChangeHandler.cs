65bac17b106141c7e5d204097144e933bdf5faa0
Hector <hector@hjs.dev>
2022-03-29 15:48:30 +0100

    feat: support system.action as change handler (#29)
    Add a new API method to allow adding a System.Action as a change handler.
    In the background this System.Action is wrapped in a class which implements
    the `IChangeHandler` interface, allowing all downstream functionality to
    continue to work in the same way.
    Add unit test to ensure any added System.Action handlers get correctly
    invoked when a change is detected.

---
+++ ExcelChangeHandler/ChangeHandling/Handler/ActionBasedChangeHandler.cs
@@ -0,0 +1,24 @@
+﻿using ExcelChangeHandler.ChangeHandling.Memory;
+using ExcelChangeHandler.Excel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelChangeHandler.ChangeHandling.Handler
+{
+    internal class ActionBasedChangeHandler<TWorksheetType, TRangeType> : IChangeHandler<TWorksheetType, TRangeType>
+        where TWorksheetType : IWorksheet where TRangeType : IRange
+    {
+        private readonly Action<IMemoryComparison, TWorksheetType, TRangeType> _handler;
+
+        public ActionBasedChangeHandler(Action<IMemoryComparison, TWorksheetType, TRangeType> handler)
+        {
+            _handler = handler ?? throw new ArgumentNullException(nameof(handler));
+        }
+
+        public void HandleChange(IMemoryComparison memoryComparison, TWorksheetType sheet, TRangeType range)
+        {
+            _handler(memoryComparison, sheet, range);
+        }
+    }
+}
